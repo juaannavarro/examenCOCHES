d5befb7a8382f349499013489f510f8725392478
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-05-12 08:38:57 +0200

    codigo entero mitad funciiones

--- prueba.py
+++ prueba.py
@@ -54,13 +54,20 @@ def recurrentes():
     return
 recurrentes()
 
-def union_tabla():
-    conversiones = pd.read_csv("conversiones.csv", sep = ";")
-    navegaciones = pd.read_csv("navegacion.csv", sep = ";")
-    data = []
-    only_user = navegacion["id_user"]
-    for user in only_user:
-        convierte = False
+recurrente = {()}
+number_of_users = {()}
+
+for i in range(navegacion.shape[0]):
+    number_of_users.add(navegacion._get_value(i, "id_user"))
+    if navegacion._get_value(i, "user_recurrent") == True:
+        recurrente.add(navegacion._get_value(i, "id_user"))
+print("El porcentaje de usuarios recurrentes frente a los totales es de:", len(recurrente) / len(number_of_users) * 100, end="%\n")
+
+#Unir a la tabla de navegacion si convierte o no.
+data = []
+only_user = navegacion["id_user"]
+for user in only_user:
+    convierte = False
     for i in conversiones["id_user"]:
         if user == i:
             convierte = True
@@ -70,29 +77,68 @@ def union_tabla():
     else:
         data.append(0)
 
-    navegacion["convierte"] = data 
-    return
-union_tabla()  
+navegacion["convierte"] = data
+#Ahora navegacio contiene todos los datos de si ese usuario convierte o no.
 
+#Ver cual es el coche más visitado de la página.
+cars = {
+    
+}
 
+for i in range(navegacion.shape[0]):
+    m = re.search("http(?:s?):\/(?:\/?)www\.metropolis\.com\/es\/(.+?)\/.*", str(navegacion._get_value(i, "url_landing")))
+    if m != None:
+        if m.groups()[0] in cars:
+            cars[m.groups()[0]] += 1
+        else:
+            cars[m.groups()[0]] = 1
+        
+for car in cars.keys():
+    print("El coche", car, "ha sido buscado", cars[car], "veces")
 
-def coches():
-    conversiones = pd.read_csv("conversiones.csv", sep = ";")
-    navegaciones = pd.read_csv("navegacion.csv", sep = ";")
-    cars={
+#Separar cada una de las partes para añair a la tabla
+campaña = []
+adg = []
+adv = []
+sl = []
+urls = navegacion["url_landing"]
+#Valor del id campaña
+for url in urls:
+    try:
+        esp = str(url).split("camp=")
+        bueno = esp[1].split("&")
+        campaña.append(bueno[0])
+    except:
+        campaña.append(0)
+#Valor del id del adgroup
+for url in urls:
+    try:
+        esp = str(url).split("adg=")
+        bueno = esp[1].split("&")
+        adg.append(bueno[0])
+    except:
+        adg.append(0)
+#valor del adv
+for url in urls:
+    try:
+        esp = str(url).split("adv=")
+        bueno = esp[1].split("&")
+        adv.append(bueno[0])
+    except:
+        adv.append(0)
+#valor del sl
+for url in urls:
+    try:
+        esp = str(url).split("sl=")
+        bueno = esp[1].split("&")
+        sl.append(bueno[0])
+    except:
+        sl.append(0)
+navegacion["id_camp"] = campaña
+navegacion["id_adg"] = adg
+navegacion["id_adv"] = adv
+navegacion["id_sl"] = sl
 
-    }
-
-    for i in range(navegaciones.shape[0]):
-        m= re.search("http(?:s?):\/(?:\/?)www\.metropolis\.com\/es\/(.+?)\/.*", str(navegacion._get_value(i, "url_landing")))
-        if m != None:
-            if m.groups()[0] in cars:
-                cars[m.groups()[0]] += 1
-            else:
-                cars[m.groups()[0]] = 1
-    for car in cars.keys():
-        print("El coche " , car,"Ha sido buscado" , cars[car], "veces")
-    return
-coches()
-
-
+print(navegacion)
+#Esto es para guardar el fichero final, comentar si no se usa
+navegacion.to_csv("navegacion_final.csv", index = False)
